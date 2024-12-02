f2e3bf14c94e7313a397464d496f133dcffe527c
Bryan LePla <bdlepla@gmail.com>
2024-12-01 21:15:38 -0500

    Using Kotlin abs and more idiomatic Kotlin.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
 
     fun parse(input:List<String>): Pair<List<Int>, List<Int>> {
@@ -11,17 +13,15 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val (firstList, secondList) = parse(input)
-        val zipped = firstList.sorted().zip(secondList.sorted())
-        val distances = zipped.map{(first, second) -> Math.abs(first - second)}
-        val ret = distances.sum()
-        return ret
+        return firstList.sorted()
+            .zip(secondList.sorted())
+            .sumOf { (first, second) -> abs(first - second) }
     }
 
     fun part2(input: List<String>): Int {
         val (firstList, secondList) = parse(input)
-        val similarityScores = firstList.map { first -> first * secondList.count{ it == first }}
-        val ret = similarityScores.sum()
-        return ret
+        return firstList
+            .sumOf { first -> first * secondList.count { it == first } }
     }
 
     val testInput = """
